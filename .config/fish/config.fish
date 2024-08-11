571a9f8f5de75c3ffdbaff33ec68560b5635918d
doanhaoCoder <152661853+doanhaoCoder@users.noreply.github.com>
2024-08-11 17:26:04 +0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,11 @@ end
 set fish_greeting ""
 
 if not test -e ~/.config/fish/temp.fish
-    touch ~/.config/fish/temp.fish
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    # Kiểm tra kết nối internet
+    if ping -c 1 8.8.8.8 > /dev/null 2>&1
+        touch ~/.config/fish/temp.fish
+        curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    else
+        echo "No internet connection. Please check your network."
+    end
 end
