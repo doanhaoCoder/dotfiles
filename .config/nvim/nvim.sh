adc0a1e5dffd90b863a9fea61790c38821e5f6c0
doanhaoCoder <doannhathao2310@gmail.com>
2024-08-12 22:29:07 +0700

    nvim.sh

---
+++ .config/nvim/nvim.sh
@@ -0,0 +1,10 @@
+sudo pacman -S nodejs \
+npm \
+python \
+python3 \
+python-pip
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+npm install neovim
