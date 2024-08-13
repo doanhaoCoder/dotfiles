35937722bdaede0e1ed016c5d9bebadd95fa3a75
doanhaoCoder <doannhathao2310@gmail.com>
2024-08-13 23:40:53 +0700

    Update nvim.sh

--- .config/nvim/nvim.sh
+++ .config/nvim/nvim.sh
@@ -2,9 +2,10 @@ sudo pacman -S nodejs \
 npm \
 python \
 python3 \
-python-pip
+python-pip \
+python-pynvim
+
+npm install neovim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-npm install neovim
