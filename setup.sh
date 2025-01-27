45ca72cbdb933622bc7d24827a8a1a6dffa13500
DoanHaoCoder <152661853+doanhaoCoder@users.noreply.github.com>
2025-01-27 10:08:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo pacman -Sy --noconfirm 
-sudo pacman -S --noconfirm git sddm fish kitty ttf-hack-nerd rofi swaybg bluez bluez-utils brightnessctl pulseaudio pavucontrol networkmanager fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey kcm-fcitx5 neovim firefox dolphin fzf btop htop vlc
+sudo pacman -Rns pipewire-pulse
+sudo pacman -S --noconfirm sddm fish kitty ttf-hack-nerd rofi swaybg bluez bluez-utils blueman brightnessctl pulseaudio pulseaudio-bluetooth pavucontrol networkmanager fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey kcm-fcitx5 neovim firefox dolphin fzf btop htop vlc
 
 sudo cp ./environment /etc/
-sudo cp -r ./.config $HOME
-sudo cp -r ./.local $HOME
+cp -r ./.config $HOME
+cp -r ./.local $HOME
 
 cd $HOME
 git clone https://aur.archlinux.org/yay.git
@@ -13,7 +14,7 @@ makepkg -si --noconfirm
 yay -S --noconfirm swaylock-effects
 
 sudo systemctl enable NetworkManager
-sudo systemctl enable bluetooth.service
+sudo systemctl enable bluetooth
 sudo systemctl enable sddm
 
 echo "Installation complete. The system will now reboot."
