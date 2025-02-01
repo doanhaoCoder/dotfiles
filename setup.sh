de717f9f70d6bbd8db949e748f842f1d83fc161c
doanhaoCoder <doannhathao2310@gmail.com>
2025-02-02 00:53:28 +0700

    update

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ sddm kitty fish ttf-hack-nerd ttf-jetbrains-mono-nerd rofi swaybg waybar \
 dunst grim slurp \
 networkmanager bluez bluez-utils blueman \
 brightnessctl pipewire wireplumber pavucontrol pamixer \
-fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey kcm-fcitx5 \ 
+fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey kcm-fcitx5 \
 dolphin nautilus unrar \
 htop btop nvtop \
-neovim ranger ueberzug fzf \
+vim nano ranger ueberzug fzf \
 firefox vlc
 
 sudo cp ./environment /etc/
@@ -23,7 +23,7 @@ mkdir Pictures
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-yay -S --noconfirm swaylock-effects cava code microsoft-edge-stable-bin
+yay -S --noconfirm swaylock-effects cava microsoft-edge-stable-bin code-git docker 
 
 # ranger devicons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
@@ -31,6 +31,7 @@ git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth
 sudo systemctl enable sddm
+sudo systemctl enable docker
 
 echo "Installation complete. The system will now reboot."
 sudo reboot
