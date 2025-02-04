5b0bfd9f299dcb489e87e21f1325c2044dd72192
DoanHaoCoder <152661853+doanhaoCoder@users.noreply.github.com>
2025-02-04 15:06:59 +0700

    update zsh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,23 @@
 sudo pacman -Sy --noconfirm
 
 sudo pacman -S --noconfirm \
-sddm kitty fish ttf-hack-nerd ttf-jetbrains-mono-nerd rofi swaybg waybar \
+sddm kitty ttf-hack-nerd ttf-jetbrains-mono-nerd rofi swaybg waybar \
 dunst grim slurp \
 networkmanager bluez bluez-utils blueman \
 brightnessctl pipewire wireplumber pavucontrol pamixer \
 fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey kcm-fcitx5 \
 dolphin nautilus unrar \
 htop btop nvtop \
-vim nano ranger ueberzug fzf \
+neovim nano ranger ueberzug fzf ark\
 firefox vlc
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+cp ./.zshrc $HOME
 
 sudo cp ./environment /etc/
 cp -r ./.config $HOME
